30b8fbac152c183644d5580ea57891406ee5c090
saadjahangir9389 <80525449+saadjahangir9389@users.noreply.github.com>
2021-08-10 03:20:52 -0700

    Update ajax.blade.php

--- ajax.blade.php
+++ ajax.blade.php
@@ -12,3 +12,37 @@
         });
     })
 </script>
+
+<-------------------------------------------->
+
+Controller Code:
+public function index()
+
+        {
+            return view('search.search');
+        }
+
+    public function search(Request $request)
+    {
+        if($request->ajax())
+        {
+            $output="";
+            $user=DB::table('login')->where('username','LIKE','%'.$request->search."%")->get();
+            if($user)
+            {
+                foreach ($user as $key => $table) {
+                    $output.=
+                        '<td>'.$table->id.'</td>'.
+                        '<td>'.$table->username.'</td>'.
+                        '<td>'.$table->password.'</td>'.
+                        '<td>'.'<button type="button" class="btn btn-warning rounded btn-xs" data-toggle="modal" data-target="#ModalLoginForm'. $table->id . '">' .
+                        'Edit </button>' . '</td>'. '</tr>';
+                } ?>
+
+
+
+                <?php return Response($output);
+            }
+        }
+    }
+}
